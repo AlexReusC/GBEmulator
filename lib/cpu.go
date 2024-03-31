9f18583ba6e1641ec8ea3b537a2fca2a951e9f97
AlexReusC <55596302+AlexReusC@users.noreply.github.com>
2024-03-30 18:05:39 -0600

    modification of cpu instructions

--- lib/cpu.go
+++ lib/cpu.go
@@ -5,6 +5,15 @@ import (
 	"fmt"
 )
 
+type flagRegister = int
+
+const (
+	z flagRegister 	= 7 	//zero flag 				-> bit 7
+	n 				= 6 	//substraction flag (BCD) 	-> bit 6
+	h 				= 5 	//half carry flag (BCD) 	-> bit 5
+	c				= 4		//carry clag				-> bit 4
+)
+
 type registers struct {
 	a  uint8
 	b  uint8
@@ -20,6 +29,7 @@ type registers struct {
 
 type CPU struct {
 	Register registers
+	CurrentData uint16
 }
 
 func LoadCpu() (*CPU, error) {
@@ -28,32 +38,53 @@ func LoadCpu() (*CPU, error) {
 	return c, nil
 }
 
-func (c *CPU) nop_00(){
+func (c *CPU) getFlag(flag flagRegister) bool {
+	return c.Register.f & (0x1 << flag) != 0
+}
+
+
+func (c *CPU) nop(){
 	fmt.Println("NOP INSTRUCTION")
 }
 
-func (c *CPU) xor_AF(){
-	fmt.Println("NOP INSTRUCTION")
+func (c *CPU) xor(){
+	fmt.Println("Xor INSTRUCTION")
 }
 
-func (c *CPU) jp_C3(b *Bus){
-	lo := uint16(b.BusRead(c.Register.pc))
-	hi := uint16(b.BusRead(c.Register.pc+1))
-	a := hi << 8 | lo
-	c.Register.pc = a
+func (c *CPU) jp(){
+	c.Register.pc = c.CurrentData
 }
 
 func (cpu *CPU) Step(b *Bus) error {
 	opcode := b.BusRead(cpu.Register.pc)
 	fmt.Printf("Opcode: %x, Pc: %x\n", opcode, cpu.Register.pc)
+	instruction, ok := instructions[opcode]
+	if !ok {
+		return errors.New("opcode not implemented")
+	}
+	fmt.Printf("Instruction: %x, Address mode: %x\n", instruction.InstructionType, instruction.AddressMode)
 	cpu.Register.pc += 1
-	switch opcode {
-		case 0x00:
-			cpu.nop_00()
-		case 0xAF:
-			cpu.xor_AF()
-		case 0xC3:
-			cpu.jp_C3(b)
+
+	//Address mode
+	switch instruction.AddressMode {
+	case am_Imp:
+		break
+	case am_N16:
+		lo := uint16(b.BusRead(cpu.Register.pc))
+		hi := uint16(b.BusRead(cpu.Register.pc+1))
+		cpu.CurrentData = hi << 8 | lo
+	}
+
+	//Conditional mode
+
+	//Instruction type
+	switch instruction.InstructionType {
+		case in_Nop:
+			cpu.nop()
+		case in_Xor:
+			cpu.xor()
+		case in_Jp:
+			cpu.jp()
 		default:
 			return errors.New("invalid instruction")
 	}
--- lib/instruction.go
+++ lib/instruction.go
@@ -1,5 +1,57 @@
 package lib
 
-func do_smth() {
+import "errors"
 
+type inType int
+type adType int
+type condType int
+
+const (
+	in_Nop inType = iota
+	in_Xor
+	in_Jp
+)
+
+const (
+	am_N16 adType = iota
+	am_Imp
+)
+
+const (
+	cond_None condType = iota
+	cond_C
+	cond_NC
+	cond_Z
+	cond_NZ
+)
+
+func checkCond(cpu *CPU, ct condType) (bool, error) {
+	c := cpu.getFlag(c)
+	z := cpu.getFlag(z)
+
+	switch ct {
+	case cond_None:
+		return true, nil
+	case cond_C:
+		return c, nil
+	case cond_NC:
+		return !c, nil
+	case cond_Z:
+		return z, nil
+	case cond_NZ:
+		return !z, nil
+	default:
+		return false, errors.New("invalid condition")
+	}
+} 
+
+type Instruction struct {
+	InstructionType inType
+	AddressMode     adType
+	ConditionType   condType
+}
+
+var instructions = map[uint8]Instruction{
+	0x00: {in_Nop, am_Imp, cond_None},
+	0xC3: {in_Jp, am_N16, cond_None},
 }
